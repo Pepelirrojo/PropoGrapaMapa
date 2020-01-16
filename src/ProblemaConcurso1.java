3a7223151cc065dab3f08185cf9c04db7d5d7b17
Pepe <jose.mateo@live.u-tad.com>
2020-01-16 12:22:28 +0100

    Hay que borrar los p**** guiones

---
+++ src/ProblemaConcurso1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ProblemaConcurso1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()) {
+			String[] frase = sc.nextLine().split("\\s");
+			for (int i = 0; i < frase.length; i++) {
+				String[] silaba = frase[i].split("-");
+				for (int j = 0; j < silaba.length; j++) {
+					char vocal = silaba[j].charAt(silaba[j].length() - 1);
+					if (vocal != 97 && vocal != 101 && vocal != 105 && vocal != 111 && vocal != 117) {
+						vocal = silaba[j].charAt(silaba[j].length() - 2);
+					}
+					String trad = silaba[j] + "p" + vocal;
+					System.out.print(trad + " ");
+				}
+			}
+		}
+	}
+}
