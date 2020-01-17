bd7611484aa9822cc029306a9530aa5c7c769392
Pepe <jose.mateo@live.u-tad.com>
2020-01-17 13:11:31 +0100

    Primer ejercicio

--- src/ProblemaConcurso1.java
+++ src/ProblemaConcurso1.java
@@ -8,13 +8,14 @@ public class ProblemaConcurso1 {
 			for (int i = 0; i < frase.length; i++) {
 				String[] silaba = frase[i].split("-");
 				for (int j = 0; j < silaba.length; j++) {
-					char vocal = silaba[j].charAt(silaba[j].length() - 1);
-					if (vocal != 97 && vocal != 101 && vocal != 105 && vocal != 111 && vocal != 117) {
+					char vocal = silaba[j].toLowerCase().charAt(silaba[j].length() - 1);
+					if (vocal != 97 && vocal != 101 && vocal != 105 && vocal != 111 && vocal != 117 && vocal != 121) {
 						vocal = silaba[j].charAt(silaba[j].length() - 2);
 					}
 					String trad = silaba[j] + "p" + vocal;
-					System.out.print(trad + " ");
+					System.out.print(trad);	
 				}
+				System.out.print(" ");
 			}
 		}
 	}
